959fdc85de21d581313697ec1cbcff1f4f9c51ce
Ryan Boehm <ryan.w.boehm@gmail.com>
2019-05-18 14:53:17 -0400

    started vector class

---
+++ vector.py
@@ -0,0 +1,16 @@
+class Vector:
+  # TODO: Finish the Vector class.
+  
+
+
+'''
+a = Vector([1, 2, 3])
+b = Vector([3, 4, 5])
+c = Vector([5, 6, 7, 8])
+
+a.add(b)      # should return a new Vector([4, 6, 8])
+a.subtract(b) # should return a new Vector([-2, -2, -2])
+a.dot(b)      # should return 1*3 + 2*4 + 3*5 = 26
+a.norm()      # should return sqrt(1^2 + 2^2 + 3^2) = sqrt(14)
+a.add(c)      # raises an exception
+'''
